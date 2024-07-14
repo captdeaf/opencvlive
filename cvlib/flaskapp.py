ede6d53e15e871d13704ad1b310fa6a5e5e3910f
Greg Millam <captdeaf@gmail.com>
2024-07-14 12:05:24 -0700

    Refactoring liveserver and flaskapp

--- cvlib/flaskapp.py
+++ cvlib/flaskapp.py
@@ -1,30 +1,43 @@
 # flaskapp.py
 #
-# Basics and utilities for flask and various paths.
+#####################################
+#
+# flaskapp.py creates a mini webserver allowing uploads and index.html.
+# Intended as a base for a more complex server.
+#
+#####################################
 
-from flask import Flask, request, redirect, url_for
-import argparse, sys, json
+# Where the html and js files are.
+STATIC_DIR  = 'html'
 
-parser = argparse.ArgumentParser(
-    prog=sys.argv[0],
-    description="Creates a mini webserver that lets you experiment with opencv's functions, live.",
+# Directories where uploads are kept. Ideally a subdirectory of STATIC_DIR
+UPLOAD_DIR     = "html/uploads"
+
+# For converting an uploaded, globbed path ("html/uploads/*") into a path
+# suitable for web client. Usually just means chopping off static folder.
+
+UPLOAD_TO_PATH = lambda p: p[len(STATIC_DIR):]
+
+
+####################################
+#
+# Begin Flask app, but don't initialize it. That's for main() or
+# importer.
+#
+####################################
+
+from flask import Flask, request, redirect, url_for, send_from_directory
+import os, sys, json
+from glob import glob
+
+app = Flask('cvliveserver',
+    static_url_path = '',
+    static_folder   = STATIC_DIR,
 )
 
-parser.add_argument('-p', '--port', default='8838', type=int)
-parser.add_argument('-b', '--bind', default="", type=str)
-parser.add_argument('-w', '--browser', action='store_true', default=False)
-
-args = parser.parse_args()
-
-BIND = args.bind
-PORT = args.port
-
-app = Flask(
-    'cvliveserver',
-    static_url_path='',
-    static_folder='html',
-)
-
+# This is ganked from another project of mine. All it really does is
+# make class objects default to __dict__. They may optionally have a
+# __json_ignore__ to remove keys from it, usually to remove nesting.
 class EasyEncoder(json.JSONEncoder):
     def default(self, o):
         if type(o) == object:
@@ -36,12 +49,68 @@ class EasyEncoder(json.JSONEncoder):
         else:
             return super().default(o)
 
-def jsondump(body, file, **kwargs):
-    json.dump(body, file, cls=EasyEncoder, **kwargs)
+# A wrapper around EasyEncoder + json
+class ejson(object):
+    def dump(*args, **kwargs):
+        return json.dump(*args, cls=EasyEncoder, **kwargs)
 
-def jsondumps(body, **kwargs):
-    return json.dumps(body, cls=EasyEncoder, **kwargs)
+    def dumps(*args, **kwargs):
+        return json.dumps(*args, cls=EasyEncoder, **kwargs)
 
+    def load(*args, **kwargs):
+        return json.load(*args, **kwargs)
+
+    def loads(*args, **kwargs):
+        return json.loads(*args, **kwargs)
+
+# Get around flask's hijacking of print(), and make it clear.
 def debug(msg):
-    print(msg, file=sys.stderr)
-    sys.stdout.flush()
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    print("!!")
+    print("!! " + msg, file=sys.stderr)
+    print("!!")
+    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    sys.stderr.flush()
+
+####################################
+#
+# Routes for stuff not directly related to CV.
+#
+####################################
+
+# Index.html
+@app.route('/')
+def static_index():
+    return send_from_directory('html', 'index.html')
+
+# Static files, including from uploads.
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory('html', path)
+
+# Upload files to the server's UPLOAD_DIR
+#
+@app.route('/upload', methods=['POST'])
+def uploadFile():
+    if request.method != 'POST':
+        return "Not Found"
+
+    try: os.mkdir(UPLOAD_DIR)
+    except: pass
+
+    for data in request.files.getlist("file"):
+        debug(data)
+        if data.filename:
+            data.save(os.path.join(UPLOAD_DIR, data.filename))
+
+    return ejson.dumps("UPLOADED")
+
+# Fetch a JSON array listing all the files in UPLOAD_DIR
+@app.route('/uploads', methods=['GET'])
+def getUploads():
+    images = [UPLOAD_TO_PATH(x) for x in glob(f"{UPLOAD_DIR}/*")]
+    return ejson.dumps(images)
+
+
+if __name__ == "__main__":
+    app.run(host='', port='8838', debug=True)
--- liveserver.py
+++ liveserver.py
@@ -7,29 +7,14 @@
 # Serves up HTML/js/etc, and the backend operations for opencv effects.
 
 import os, sys, time, threading, argparse
-import webbrowser, json
-
-from flask import send_from_directory
 
 cwd = os.getcwd()
 if cwd not in sys.path:
     sys.path.append(cwd)
 
-from cvlib.flaskapp import app, jsondump, debug
+from cvlib.flaskapp import app
 
-@app.route('/')
-def main_index():
-    return send_from_directory('html', 'index.html')
-
-@app.route('/<path:path>', defaults={'file': 'index.html'})
-def static_files(path):
-    if os.path.isfile(path + "/index.html"):
-        return send_from_directory('html', path + "/index.html")
-    return send_from_directory('html', path)
-
-import cvlib.uploads
-
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(
         prog=sys.argv[0],
         description="Creates a mini webserver that lets you experiment with opencv's functions, live.",
@@ -41,20 +26,21 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    BIND = args.bind
-    PORT = args.port
-
     if args.browser:
+        import webbrowser
         def openBrowser():
             try:
                 time.sleep(2)
-                webbrowser.open(f"http://{BIND}:{PORT}", new=1)
+                webbrowser.open(f"http://{args.bind}:{args.port}", new=1)
             except:
                 debug("Unable to open browser", file=sys.stderr)
         browserthread = threading.Thread(target=openBrowser)
         browserthread.start()
 
     try:
-        app.run(host=BIND, port=PORT, debug=True)
+        app.run(host=args.bind, port=args.port, debug=True)
     except KeyboardInterrupt:
         pass
+
+if __name__ == "__main__":
+    main()
